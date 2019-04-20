1d493704616645a7799bc6c8b9f0410ac2c880fd
gauravjain94 <jain.g@husky.neu.edu>
2019-04-20 19:35:16 -0400

    Trying yelp

--- data/models/user/user.model.server.js
+++ data/models/user/user.model.server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const userSchema = require('./user.schema.server');
-const userModel = mongoose.model('UserModel', userSchema);
+const userModel = require('./user.model.server')
 
 findAllUsers = () =>
     userModel.find({});
